2eca4297e99483a4519886e4d26a2a4541256deb
khang0708 <40711260+khang0708@users.noreply.github.com>
2019-07-25 15:30:14 +0700

    Update Exam2.js

--- Exam2.js
+++ Exam2.js
@@ -1,36 +1,74 @@
-function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
 
-async function starttask(item) {
-  await delay(2000);
-  console.log('Task ' + item + ' Done in ' + item*2 +'s');
+const run = async (numConcurrentTasks, getTask) => {
+  var listTask=[];
+   async function res(){
+     return new Promise((resolve,reject)=>{
+       //Run 3 task cùng lúc
+       for(let i=0;i<numConcurrentTasks;i++){
+         let a = getTask();
+        resolve( listTask.push(a()));
+       }
+     })     
+    }
+   res().then(
+    await Promise.all(listTask.map(item=>{
+    return item.then(async ()=>{
+      //Chạy các task còn lại
+      for(let i = numConcurrentTasks;i<10;i++)
+      {
+        let a=getTask();
+        if(a !== undefined){
+         await a();
+        }      
+      }
+    })  
+  })))
+  .catch(e =>console.log(e));
+};
 
-}
-var startedAt=0;
-async function donetask(item){
-  await delay(2000);
-  console.log('Task ' + item + ' Done');
-}
-async function startarray(array) {
-   startedAt = Date.now();
-  for (const item of array) {
-    await starttask(item);
+/**
+* This function is used to stimulate task processing time
+*/
+const waitFor = ms => {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(), ms);
+  });
+};
+
+/**
+* Main
+*/
+const main = async () => {
+  const startedAt = Date.now();
+
+  // Define 10 tasks, task i_th would take i*2 seconds to finish
+  const tasks = [];
+  for (let i = 1; i <= 10; i++) {
+    const task = async () => {
+      console.log(`Task ${i} started, done in ${i*2}s`);
+      await waitFor(i * 2000);
+      console.log(`Task ${i} DONE!`);
+    };
+    tasks.push(task);
   }
 
-}
-async function donearray(array) {
-  for (const item of array) {
-    await donetask(item);
-  }
- let totaltime =Date.now() - startedAt;
-  console.log('Done after '+ totaltime + ' ms')
-}
-async function main(){
+  console.log('Processing 3 tasks concurrently');
+  // Run 3 tasks concurrently
+  await run(3, () => {
+    // Shift one task from the list, this task should be queued up for processing
+    const task = tasks.shift();
 
-  startarray([1, 2, 3,4,5,6,7,8,9,10])
-  await delay(6000)
-  donearray([1,2,3,4,5,6,7,8,9,10])
-}
+    if (!task) return;
 
-main();
+    // return the task for queueing
+
+    return task;
+    
+  });
+  
+   console.log(`DONE after ${Date.now() - startedAt}ms`)
+};
+
+main().catch(e => console.log(e.stack));
+
+
